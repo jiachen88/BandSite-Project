64b49ac4e0fd8d4df2db2622148d86c8758c5517
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-10-24 03:32:16 -0700

    shows setup

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,7 @@
+const showTime = document.getElementById('showTime')
+const showTimeContainer = [];
+const shows = new Set(["date", "venue", "location"])
+show.date = "Mon Sept 06 2021"
+show.venue = "Ronald Lane"
+show.location = "San Francisco, CA"
+console.log("hello world")
