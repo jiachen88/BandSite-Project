8f9db1196442aee6ff4716b36ca18c469ac4ef97
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-10-24 01:42:29 -0700

    Comments updated

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,17 +3,17 @@ const biographyComments = document.getElementById('biographyComments')
 const biographyCommentsEntries = document.getElementById('biographyCommentsEntry')
 const biographyCommentsContainer = [];
 const imageHolder = document.getElementById('commentsImage')
+const dateHolder = document.getElementById('commentsDate')
+const bigContainer = document.getElementById('largeContainer')
 ///gather date
 const currentDate = new Date()
 var day = currentDate.getDate()
 var month = currentDate.getMonth() + 1
 var year = currentDate.getFullYear()
 const dateForm = (day + "/" + month + "/" + year);
-
-
 //Renders new comments
 function renderComments() {
-    biographyCommentsEntries.innerText = '';
+    bigContainer.innerText = '';
     for (let i = 0; i < biographyCommentsContainer.length; i++) {
         console.log(biographyCommentsContainer);
         //Create elements
@@ -22,6 +22,11 @@ function renderComments() {
         const biographyListComment = document.createElement('p');
         const biographyListImage = document.createElement('img')
         const biographyDate = document.createElement('p')
+        const bigContainer = document.createElement('div')
+        const unorderedList = document.createElement('ul')
+        const commentHolder = document.createElement('div')
+        const imageContainer = document.createElement('div')
+        const biographyDateContainer = document.createElement('div')
         ///gather date
         const currentDate = new Date()
         var day = currentDate.getDate()
@@ -30,10 +35,13 @@ function renderComments() {
         const dateForm = (day + "/" + month + "/" + year);
 
         //adding classes to elements
+        bigContainer.classList.add('biography-comments__flex')
+        commentHolder.classList.add('biography-comments__holder')
         biographyListArticle.classList.add('biography-comments__item')
         biographyListName.classList.add('biography-comments__commenter');
         biographyListComment.classList.add('biography-comments__content');
         biographyListImage.classList.add('biography-comments__images');
+        biographyDate.classList.add('biography-comments__date')
         //image and text
         biographyListImage.src = '../assets/Images/Mohan-muruge.jpg'
         biographyListName.innerText = biographyCommentsContainer[i].newCommentName;
@@ -41,11 +49,16 @@ function renderComments() {
         biographyDate.innerText = dateForm;
 
         //inserting elements into correct divs
-        imageHolder.appendChild(biographyListImage);
-        biographyCommentsEntries.appendChild(biographyListArticle);
+        largeContainer.appendChild(bigContainer);
+        bigContainer.appendChild(commentHolder);
+        commentHolder.appendChild(imageContainer);
+        imageContainer.appendChild(biographyListImage);
+        commentHolder.appendChild(unorderedList);
+        unorderedList.appendChild(biographyListArticle);
         biographyListArticle.appendChild(biographyListName);
         biographyListArticle.appendChild(biographyListComment);
-        biographyListArticle.appendChild(biographyDate)
+        bigContainer.appendChild(biographyDateContainer)
+        biographyDateContainer.appendChild(biographyDate)
     }
     console.log(biographyCommentsContainer)
 }
@@ -64,3 +77,4 @@ biographyComments.addEventListener('submit', function (event) {
     //runs render comments function
     renderComments();
 })
+
