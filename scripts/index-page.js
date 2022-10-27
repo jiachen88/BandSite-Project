fd557737d929ffe20a9519d0fd586f821a7f375a
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-10-27 16:05:11 -0700

    setting up function

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -7,50 +7,50 @@ axios.get(commentsApi).then(response => {
     //Declaring Const for comments
     let commentsContainer = document.getElementById('largeContainer')
     //for loop for comments
-    for (let i = 0; i < commentsArray.length; i++) {
-        console.log(commentsArray[i].comment);
-        //creating elements for comments to be inserted to
-        let smallContainer = document.createElement('div')
-        let imageContainer = document.createElement('div');
-        let imageContent = document.createElement('img');
-        let commenterContainer = document.createElement('div');
-        let commenterName = document.createElement('h2');
-        let commenterComment = document.createElement('p');
-        let dateContainer = document.createElement('div');
-        let dateContent = document.createElement('p');
-        //inserting content
-        imageContent.src = '../assets/images/Mohan-muruge.jpg';
-        commenterName.innerText = commentsArray[i].name;
-        commenterComment.innerText = commentsArray[i].comment;
-        dateContent.innerText = new Date(commentsArray[i].timestamp).toLocaleDateString();
-        //adding classes to elements
-        smallContainer.classList.add('biography-comments__container-small')
-        commenterContainer.classList.add('biography-comments__holder')
-        imageContent.classList.add('biography-comments__images')
-        dateContent.classList.add('biography-comments__date')
-        //appending the elements to correct divs
-        commentsContainer.appendChild(smallContainer);
-        smallContainer.appendChild(imageContainer)
-        imageContainer.appendChild(imageContent);
-        smallContainer.appendChild(commenterContainer);
-        commenterContainer.appendChild(commenterName);
-        commenterContainer.appendChild(commenterComment);
-        smallContainer.appendChild(dateContainer);
-        dateContainer.appendChild(dateContent);
+    function renderComments() {
+        for (let i = 0; i < commentsArray.length; i++) {
+            console.log(commentsArray[i].comment);
+            //creating elements for comments to be inserted to
+            let smallContainer = document.createElement('div')
+            let imageContainer = document.createElement('div');
+            let imageContent = document.createElement('img');
+            let commenterContainer = document.createElement('div');
+            let commenterName = document.createElement('h2');
+            let commenterComment = document.createElement('p');
+            let dateContainer = document.createElement('div');
+            let dateContent = document.createElement('p');
+            //inserting content
+            imageContent.src = '../assets/images/Mohan-muruge.jpg';
+            commenterName.innerText = commentsArray[i].name;
+            commenterComment.innerText = commentsArray[i].comment;
+            dateContent.innerText = new Date(commentsArray[i].timestamp).toLocaleDateString();
+            //adding classes to elements
+            smallContainer.classList.add('biography-comments__container-small')
+            commenterContainer.classList.add('biography-comments__holder')
+            commenterName.classList.add
+            imageContent.classList.add('biography-comments__images')
+            dateContent.classList.add('biography-comments__date')
+            //appending the elements to correct divs
+            commentsContainer.appendChild(smallContainer);
+            smallContainer.appendChild(imageContainer)
+            imageContainer.appendChild(imageContent);
+            smallContainer.appendChild(commenterContainer);
+            commenterContainer.appendChild(commenterName);
+            commenterContainer.appendChild(commenterComment);
+            smallContainer.appendChild(dateContainer);
+            dateContainer.appendChild(dateContent);
+        }
+        //submit button event listener
+        biographyComments.addEventListener('submit', function (event) {
+            event.preventDefault();
+            axios.post("https://project-1-api.herokuapp.com/comments?api_key=de986b5f-41ca-426b-b1b5-bcad86d9fbe7",
+                {
+                    name: event.target.name.value,
+                    comment: event.target.comment.value
+                }
+            );
+            renderComments()
+        });
     }
-    //submit button event listener
-    biographyComments.addEventListener('submit', function (event) {
-        event.preventDefault();
-        console.log('comment posted');
-        //targets the text in the form
-        const newCommentName = event.target.name.value;
-        const newCommentContent = event.target.comment.value;
-        axios.post("https://project-1-api.herokuapp.com/comments?api_key=de986b5f-41ca-426b-b1b5-bcad86d9fbe7",
-            {
-                name: 'Hi',
-                comment: 'hello'
-            });
-    });
-
 })
 
